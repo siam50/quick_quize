dfb4f40e182df4c19e340a499f3063255bcb5923
siam50 <siamurrahman50@gmail.com>
2022-10-11 14:28:50 +0600

    barChart Added

--- src/components/Statistics/Statistics.js
+++ src/components/Statistics/Statistics.js
@@ -1,9 +1,23 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import { Bar, BarChart, ResponsiveContainer, Tooltip, XAxis, YAxis } from 'recharts';
 
 const Statistics = () => {
+    const [quizes, setQuizes] = useState([])
+    useEffect(() => {
+        axios.get('https://openapi.programming-hero.com/api/quiz')
+            .then(data => setQuizes(data.data.data))
+    }, [])
+    console.log(quizes)
     return (
         <div>
-            <h1>Static Page</h1>
+            <h1 className='text-3xl font-semibold mb-5'>Here is Total Quize Statistics</h1>
+            <BarChart width={350} height={300} data={quizes}>
+                <Bar dataKey="total" fill="#8884d8" />
+                <XAxis dataKey="name" />
+                <YAxis />
+                <Tooltip />
+            </BarChart>
         </div>
     );
 };
